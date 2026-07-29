cfdbf837abdb86fc9eb0a4921ad45e16c6629f02
Manasi Variar <manasirvariar13121999@gmail.com>
2026-07-28 23:30:11 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(char for char in s if char.isalnum()).lower()
+        print(s)
+        i, j = 0, len(s)-1
+        while i<j:
+            if s[i] == s[j]:
+                print(i, j)
+                i+=1
+                j-=1
+            else:
+                return False
+        
+        return True
