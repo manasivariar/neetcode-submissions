cab277752ec4428f5ae3593152501bcee8f5e9ad
Manasi Variar <manasirvariar13121999@gmail.com>
2026-07-30 22:11:41 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) > len(s2):
+            return False
+
+        s1_freq, s2_freq = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1_freq[ord(s1[i])-ord('a')]+=1
+            s2_freq[ord(s2[i])-ord('a')]+=1
+
+        if s1_freq == s2_freq:
+                return True
+
+        for i in range(len(s1), len(s2)):
+        
+            s2_freq[ord(s2[i]) - ord('a')] += 1
+            s2_freq[ord(s2[i - len(s1)]) - ord('a')] -= 1
+        
+            if s1_freq == s2_freq:
+                return True
+
+        return False
+        
+
+
+        
+
+
+
+        
+        
